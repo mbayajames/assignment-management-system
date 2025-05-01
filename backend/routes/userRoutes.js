1c5dfbcb7489b050e6a2d62fd129c0e59cee29c3
mbayajames <jmuthiora244@gmail.com>
2025-05-01 07:11:04 -0700

    Add authentication and authorization middleware to user routes in userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const userController = require('../controllers/userController');
-const auth = require('../middleware/auth');
+const userController = require("../controllers/userController");
+const auth = require("../middleware/auth");
 
-router.post('/', auth, userController.createUser);
-router.get('/', auth, userController.getAllUsers);
+router.post("/", auth, userController.createUser);
+router.get("/", auth, userController.getAllUsers);
 
 module.exports = router;

651658e0f6e18b5998e5f3c48fdf8e57445ae2bf
mbayajames <jmuthiora244@gmail.com>
2025-05-01 07:00:37 -0700

    Update DB host and credentials in dbConfig.js

--- backend/config/dbConfig.js
+++ backend/config/dbConfig.js
@@ -1,21 +1,21 @@
-const { sequelize } = require('sequelize');
+const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        dialect: 'mysql',
-        pool: {
-            max: 5, 
-            min: 0,
-            acquire: 30000,
-            idle: 10000
-        },
-        logging: false
-    };
-)
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    },
+    logging: false
+  }
+);
 
 module.exports = sequelize;

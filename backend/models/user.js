2cfb5420c306e634bde53a7fd26b28f558004fb3
mbayajames <jmuthiora244@gmail.com>
2025-05-01 07:09:34 -0700

    Update password validation in user.js model

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,27 +1,31 @@
 module.exports = (sequelize, DataTypes) => {
-    const User = sequelize.define('User', {
-        id: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true
-        },
-        username: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-        },
-        password: {
-            type: DataTypes.ENUM('admin', 'student'),
-            allowNull: false
-        },
-        role: {
-            type: DataTypes>ENUM('admin', 'student')
-            allowNull: false
-        }
-    }, {
-        tableName: 'users',
-        timestamps: true
-    });
+  const User = sequelize.define(
+    "User",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      role: {
+        type: DataTypes.ENUM("admin", "student"),
+        allowNull: false,
+      },
+    },
+    {
+      tableName: "users",
+      timestamps: true,
+    }
+  );
 
-    return User;
+  return User;
 };

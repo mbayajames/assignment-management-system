3dea7ffe4e8f5a0e9c24e728816d0dff8b43fbb9
mbayajames <jmuthiora244@gmail.com>
2025-05-01 07:08:43 -0700

    Add new User model import and association in index.js

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,32 +1,39 @@
-const { Sequelize } = require('sequelize');
-const sequelize = require('../config/dbConfig');
+const { Sequelize } = require("sequelize");
+const sequelize = require("../config/dbConfig");
 
 const db = {};
 
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-//import models
-db.User = require('./user')(sequelize, Sequelize);
-db.Assignment = require('./assignment')(sequelize, Sequelize);
-db.Submission = require('./submission')(sequelize, Sequelize);
+// Import models
+db.User = require("./user")(sequelize, Sequelize);
+db.Assignment = require("./assignment")(sequelize, Sequelize);
+db.Submission = require("./submission")(sequelize, Sequelize);
 
-//relationships
-db.User.hasMany(db.Assignment, { foreignKey: 'createdBy' });
-db.Assignment.belongsTo(db.User, { foreignKey: 'createdBy' });
+// Define relationships
+db.User.hasMany(db.Assignment, { foreignKey: "createdBy" });
+db.Assignment.belongsTo(db.User, { foreignKey: "createdBy" });
 
-db.User.hasMany(db.Submission, { foreignKey: 'studentId' });
-db.Submission.belongsTo(db.User, { foreignKey: 'studentId' });
+db.User.hasMany(db.Submission, { foreignKey: "studentId" });
+db.Submission.belongsTo(db.User, { foreignKey: "studentId" });
 
-db.Assignment.hasMany(db.Submission, { foreignKey: 'assignmentId' });
-db.Submission.belongsTo(db.Assignment, { foreignKey: 'assignmentId' });
+db.Assignment.hasMany(db.Submission, { foreignKey: "assignmentId" });
+db.Submission.belongsTo(db.Assignment, { foreignKey: "assignmentId" });
 
-db.User.belongsToMany(db.Assignment, { through: 'AssignmentUsers', foreignKey: 'userId' });
-db.Assignment.belongsToMany(db.User, { through: 'AssignmentUsers', foreignKey: 'assignmentId' });
+db.User.belongsToMany(db.Assignment, {
+  through: "AssignmentUsers",
+  foreignKey: "userId",
+});
+db.Assignment.belongsToMany(db.User, {
+  through: "AssignmentUsers",
+  foreignKey: "assignmentId",
+});
 
-//Sync database
-db.sequelize.sync({ alter: true })
-   .then(() => console.log('Database synced'))
-   .catch(err => console.error('Error syncing database:', err));
+// Sync database
+db.sequelize
+  .sync({ alter: true })
+  .then(() => console.log("Database synced"))
+  .catch((err) => console.error("Error syncing database:", err));
 
 module.exports = db;

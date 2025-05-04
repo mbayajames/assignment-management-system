6ae20bb52e2774b4bc08dde57f6b0b1476105108
mbayajames <jmuthiora244@gmail.com>
2025-05-04 06:51:06 -0700

    Enhancement: Improved error response formatting in utils/response.js

---
+++ backends/utils/response.js
@@ -0,0 +1,9 @@
+const successResponse = (res, data, status = 200) => {
+  res.status(status).json({ success: true, data });
+};
+
+const errorResponse = (res, message, status = 400) => {
+  res.status(status).json({ success: false, message });
+};
+
+module.exports = { successResponse, errorResponse };

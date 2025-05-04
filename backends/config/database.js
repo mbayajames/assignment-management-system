21746fa24ac049f0ca823ff6b89683b734db4bbc
mbayajames <jmuthiora244@gmail.com>
2025-05-04 04:30:50 -0700

    Update database.js with new database connection configurations

---
+++ backends/config/database.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+module.exports = mongoose;

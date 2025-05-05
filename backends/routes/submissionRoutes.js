6cfd247c7c93f9fbcba6b4374628edba99f49e49
mbayajames <jmuthiora244@gmail.com>
2025-05-05 14:41:21 -0700

    Update submissionRoutes.js to improve route handling and controller integration

--- backends/routes/submissionRoutes.js
+++ backends/routes/submissionRoutes.js
@@ -4,9 +4,15 @@ const auth = require("../middleware/auth");
 const {
   getSubmissions,
   createSubmission,
+  updateSubmission,
+  markSubmission,
+  deleteSubmission,
 } = require("../controllers/submissionController");
 
 router.get("/", auth, getSubmissions);
 router.post("/", auth, createSubmission);
+router.patch("/:submissionId", auth, updateSubmission);
+router.patch("/:submissionId/status", auth, markSubmission);
+router.delete("/:submissionId", auth, deleteSubmission);
 
 module.exports = router;

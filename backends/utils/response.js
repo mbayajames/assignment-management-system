8a22c88e4f6f66e28e1561d0b09863c2dfcd6155
mbayajames <jmuthiora244@gmail.com>
2025-05-17 14:32:02 -0700

    refactor: delete obsolete response.js utility from backend utils

--- backends/utils/response.js
+++
@@ -1,9 +0,0 @@
-const successResponse = (res, data, status = 200) => {
-  res.status(status).json({ success: true, data });
-};
-
-const errorResponse = (res, message, status = 400) => {
-  res.status(status).json({ success: false, message });
-};
-
-module.exports = { successResponse, errorResponse };

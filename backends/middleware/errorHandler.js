5c74f1b589f0f44dfd4301c6ee1640cec518a70a
mbayajames <jmuthiora244@gmail.com>
2025-05-17 14:41:07 -0700

    feat: add new chnanges to the modules

--- backends/middleware/errorHandler.js
+++
@@ -1,8 +0,0 @@
-const logger = require("../utils/logger");
-
-const errorHandler = (err, req, res, next) => {
-  logger.error(err.message, { stack: err.stack });
-  res.status(500).json({ message: "Server error" });
-};
-
-module.exports = errorHandler;

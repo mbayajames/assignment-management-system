565d321e88902444e35293b46cf679bd69b332e8
mbayajames <jmuthiora244@gmail.com>
2025-05-18 09:17:45 -0700

    chore: update user management logic in userController.js

---
+++ backends/controllers/userController.js
@@ -0,0 +1,38 @@
+const User = require('../models/user');
+
+exports.getAllUsers = async (req, res, next) => {
+  try {
+    const users = await User.find().select('-password');
+    res.json(users);
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.getUserById = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id).select('-password');
+    if (!user) return res.status(404).json({ msg: 'User not found' });
+    res.json(user);
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.updateUser = async (req, res, next) => {
+  try {
+    const updated = await User.findByIdAndUpdate(req.params.id, req.body, { new: true }).select('-password');
+    res.json(updated);
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.deleteUser = async (req, res, next) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.json({ msg: 'User deleted successfully' });
+  } catch (err) {
+    next(err);
+  }
+};
